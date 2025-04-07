d7427781e722a9ac13bc7934c1d3d16de24b197b
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-04-07 20:22:30 +0300

    my fix task1

--- src/main/java/ru/otus/java/basic/git/Application.java
+++ src/main/java/ru/otus/java/basic/git/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
+        System.out.println(-2);
         System.out.println(3);
     }
 
