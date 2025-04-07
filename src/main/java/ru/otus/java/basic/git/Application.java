dd7604dc66224ccf97d4d092df006af69652111a
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-04-07 20:18:11 +0300

    task 1 completed

--- src/main/java/ru/otus/java/basic/git/Application.java
+++ src/main/java/ru/otus/java/basic/git/Application.java
@@ -3,5 +3,12 @@ package ru.otus.java.basic.git;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        doSomething();
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
