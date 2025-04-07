a58558ac33c8c3cb50d2f88be616288b26e0c599
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-04-07 20:21:40 +0300

    task 1 recovered

--- src/main/java/ru/otus/java/basic/git/Application.java
+++ src/main/java/ru/otus/java/basic/git/Application.java
@@ -6,6 +6,12 @@ public class Application {
         task2();
     }
 
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+
     public static void task2() {
         System.out.println("---");
         System.out.println("- -");
