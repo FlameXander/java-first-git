2f3d4761fcb7bb22a10887c8bee83a4bb9737b24
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-04-07 21:25:27 +0300

    hw1 fix

--- src/main/java/ru/otus/java/basic/git/Application.java
+++ src/main/java/ru/otus/java/basic/git/Application.java
@@ -29,6 +29,8 @@ public class Application {
     public static void homework1() {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
         System.out.println(5);
     }
 }
